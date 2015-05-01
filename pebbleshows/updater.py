46e1e46c65d63482f419824a190dc70e304218b3
Alessio Bogon <youtux@gmail.com>
2015-05-01 02:43:05 +0200

    Concurrent requests using python's concurrent module

--- pebbleshows/updater.py
+++ pebbleshows/updater.py
@@ -3,23 +3,27 @@ import datetime
 import os
 import random
 import logging
-import collections
+import concurrent.futures
 
 import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+import pypebbleapi
+
 
 from trakttv import Trakttv
 from pin_database import PinDatabase
-import pebble
 
 
 TRAKTV_CLIENT_ID = os.environ["TRAKTV_CLIENT_ID"]
 MONGODB_URL = os.environ["MONGODB_URL"]
 PEBBLE_TIMELINE_API_KEY = os.environ["PEBBLE_TIMELINE_API_KEY"]
 
-ObjectResult = collections.namedtuple("ObjectResult",
-    ("pin", "metadata", "future"))
+timeline = pypebbleapi.Timeline(api_key=PEBBLE_TIMELINE_API_KEY)
+trakttv = Trakttv(TRAKTV_CLIENT_ID)
+pins_db = PinDatabase(MONGODB_URL)
+
+l = logging.getLogger("scheduler")
 
 
 def create_episode_pin(
@@ -31,12 +35,17 @@ def create_episode_pin(
         episode_number,
         episode_title,
         ):
+    if not show_title:
+        show_title = "Unknown show"
+
     season_episode_str = "S{season:0>2}E{episode:0>2}".format(
         season=season_number, episode=episode_number)
-    if show_title:
-        pin_title = "{} | {}".format(show_title, season_episode_str)
-    else:
-        pin_title = season_episode_str
+
+    # pin_title = show_title
+    pin_title = "{} | {}".format(show_title, season_episode_str)
+
+    if not episode_title:
+        episode_title = ""
 
     pin = {
         "id": pin_id,
@@ -44,8 +53,11 @@ def create_episode_pin(
         "duration": duration,
         "layout": {
             "type": "calendarPin",
+            # "shortTitle": pin_short_title,
             "title": pin_title,
+            # "subtitle": season_episode_str,
             "body": episode_title,
+            "tinyIcon": "system://images/MOVIE_EVENT",
         },
         "actions": [
             {
@@ -63,19 +75,30 @@ def create_episode_pin(
     # Remove None values
     if pin["duration"] is None:
         del pin["duration"]
-    if pin["layout"]["body"] is None:
+    if not pin["layout"]["body"]:
         del pin["layout"]["body"]
 
     return pin
 
 
+def send_and_log_pin(pin, topics, metadata):
+    response = timeline.send_shared_pin(topics, pin)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        l.error(e)
+        l.error(response.content)
+        return
+    pins_db.upsert(pin, metadata)
+
+    l.info("Pin (id={}, title={}) sent and updated.".format(
+        pin["id"],
+        pin["layout"].get("title", None),
+        )
+    )
+
+
 def fetch_shows_and_send_pins():
-    l = logging.getLogger("scheduler")
-
-    timeline = pebble.Timeline(api_key=PEBBLE_TIMELINE_API_KEY)
-    trakttv = Trakttv(TRAKTV_CLIENT_ID)
-    pins_db = PinDatabase(MONGODB_URL)
-
     today = datetime.date.today()
     start_date = today - datetime.timedelta(days=3)
 
@@ -89,49 +112,39 @@ def fetch_shows_and_send_pins():
         return
 
     pin_ids_already_sent = pins_db.all_pin_ids()
-    #TODO use pebble (on pypi, nothing to do with this file)
-    pin_insertion_results = []
 
-    for ep_schedule in calendar:
-        episode_id = ep_schedule["episode"]["ids"]["trakt"]
-        show_id = ep_schedule["show"]["ids"]["trakt"]
-        pin_id = "schedule-{episode_id}".format(episode_id=episode_id)
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        # Start the load operations and mark each future with its URL
 
-        if pin_id in pin_ids_already_sent:
-            l.debug("Pin (id={}) already sent. skipping.".format(pin_id))
-            continue
+        for ep_schedule in calendar:
+            episode_id = ep_schedule["episode"]["ids"]["trakt"]
+            show_id = ep_schedule["show"]["ids"]["trakt"]
+            pin_id = "schedule-{episode_id}".format(episode_id=episode_id)
 
-        pin = create_episode_pin(
-            pin_id=pin_id,
-            date=ep_schedule["first_aired"],
-            duration=60,
-            show_title=ep_schedule["show"]["title"],
-            season_number=ep_schedule["episode"]["season"],
-            episode_number=ep_schedule["episode"]["number"],
-            episode_title=ep_schedule["episode"]["title"],
-        )
-        metadata = {
-            'episodeID': episode_id,
-        }
+            if pin_id in pin_ids_already_sent:
+                l.debug("Pin (id={}) already sent. skipping.".format(pin_id))
+                continue
 
-        future = timeline.send_shared_pin(pin, [show_id])
-        pin_insertion_results.append(ObjectResult(pin, metadata, future))
-
-    for pr in pin_insertion_results:
-        response = pr.future.result()
-        try:
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            l.error(e)
-            return
-        pins_db.upsert(pr.pin, pr.metadata)
-
-        l.info("Pin (id={}, title={}) sent and updated.".format(
-            pr.pin["id"],
-            pr.pin["layout"].get("title", None),
+            pin = create_episode_pin(
+                pin_id=pin_id,
+                date=ep_schedule["first_aired"],
+                duration=40,
+                show_title=ep_schedule["show"]["title"],
+                season_number=ep_schedule["episode"]["season"],
+                episode_number=ep_schedule["episode"]["number"],
+                episode_title=ep_schedule["episode"]["title"],
             )
-        )
-        l.debug(pin)
+            metadata = {
+                'episodeID': episode_id,
+            }
+            # send_and_log_pin(topics=[show_id], pin=pin, metadata=metadata)
+            futures.append(executor.submit(
+                send_and_log_pin, topics=[show_id], pin=pin, metadata=metadata)
+            )
+        for _ in concurrent.futures.as_completed(futures):
+            pass
+
     print("End of the job")
 
 
